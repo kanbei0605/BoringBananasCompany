ace0581c1a12b7298fff710d9f82ebb54de0028d
kanbei0605 <kanbei0605@gmail.com>
2022-03-29 00:45:26 +0900

    changed coding style

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import Head from 'next/head'
 
 export default function Home() {
-
   return (
     <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Boring Bananas Company</title>
         <link rel="icon" href="/images/favicon.jpg" />
-
         <meta property="og:title" content="Boring Bananas Co." key="ogtitle" />
         <meta property="og:description" content="Here at Boring Bananas company, we specialise in the world's finest digital bananas. We've put together a team spanning 3 continents, to bring you some of the most ‍NUTRITIOUS and DELICIOUS bananas out known to man." key="ogdesc" />
         <meta property="og:type" content="website" key="ogtype" />
         <meta property="og:url" content="https://boringbananas.co/" key="ogurl"/>
         <meta property="og:image" content="https://boringbananas.co/images/Hola.gif" key="ogimage"/>
         <meta property="og:site_name" content="https://boringbananas.co/" key="ogsitename" />
-
         <meta name="twitter:card" content="summary_large_image" key="twcard"/>
         <meta property="twitter:domain" content="boringbananas.co" key="twdomain" />
         <meta property="twitter:url" content="https://boringbananas.co/" key="twurl" />
@@ -35,11 +32,8 @@ export default function Home() {
               <a href="https://twitter.com/boringbananasco" className="text-4xl  hover:text-white m-6 text-blau">TWITTER</a>
               <a href="https://discord.gg/8Wk9Hp6UyV" className="text-4xl  hover:text-white m-6 text-blau">DISCORD</a>
             </nav>
-             
           </div>
-          
         </div>
-
         <div className="md:w-2/3 w-4/5 " id="about">
           <div className="mt-6 border-b-2 py-6">
             <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
@@ -72,24 +66,20 @@ export default function Home() {
                     </p>
                   </div>
                 </div>
-                
                 <div id="gallery" className="   mx-6 py-6">
                     <h2 className="text-blau Poppitandfinchsans text-7xl text-center">SIGNATURE SERIES GALLERY</h2>
                   <div className="flex flex-wrap  items-center mx-6   py-6 ">
                     <div className="md:w-1/2">
                       <div className=" border-blue-300 p-2"><img src="images/gallery1.jpeg" alt="" width="600px" className="feature-image"/></div>
-
                     </div>
                     <div className="md:w-1/2 flex flex-wrap">
                       <div className=" p-2 w-1/2"><img src="images/gallery2.jpeg" alt="" width="400px" className="feature-image"/></div>
                       <div className=" p-2 w-1/2"><img src="images/gallery3.jpeg" alt="" width="400px" className="feature-image"/></div>
                       <div className=" p-2 w-1/2"><img src="images/gallery4.jpeg" alt="" width="400px" className="feature-image"/></div>
                       <div className=" p-2 w-1/2"><img src="images/gallery5.jpeg" alt="" width="400px" className="feature-image"/></div>
-
                     </div>
                   </div>
                 </div>
-
               <div id="roadmap" className="">
                 <img src="/images/Roadmap.png"/>
               <h2 className="text-7xl text-center Poppitandfinchsans text-blau my-4">OUR MISSION STATEMENT</h2>
@@ -108,16 +98,12 @@ export default function Home() {
                 <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> 4 x 10 ETH donations: </span> The first of these will be made to <a target="_blank" href="https://girlswhocode.com/" className="underline text-black font-bold">Girls Who Code</a>, working to empower young women and <span className="font-bold"> close the gender gap </span> in technology. We know women are unrepresented in the NFT space - lets help to fix this! The remaining donations will be made via community vote using <a target="_blank" href="https://thegivingblock.com/" className="underline text-black font-bold">the Giving Block</a>.</li>
                 <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> 20 ETH to Community Wallet: </span> Directed towards supporting creators that want to make a positive contribution to the NFT space.</li>
                 <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> Secondary sales: 5% royalty </span> (2.5% donation, 2.5% to the team). Secondary sales are directed towards charities voted on by the community monthly.</li>
-
               </ul>
               <p className="text-xl text-white my-6  montserrat"> In addition, we have been, and continue to make <span className="font-bold underline"> high quality content for the NFT community </span> through our <span className="font-bold underline"> PROJECT IN PROGRESS </span> series, to help budding creators to along their journey. 
               </p>
               <p className="text-xl text-white my-6  montserrat"> All fully annotated source code for image generation and the smart contract will be made available after launch as a community resource. 
               </p>
-
-
               </div>
-              
               <div id="team" className="mx-12 my-16 ">
                 <h2 className="text-7xl text-center Poppitandfinchsans text-blau my-4">BANANA RESEARCH TEAM</h2>
                 <div className="flex justify-around flex-wrap">
@@ -142,11 +128,7 @@ export default function Home() {
                     <div className="cards-image-mask"><img src="/images/Berk.png" width="360px" alt="" className="cards-image" /></div>
                     <h3 className="my-4 text-center text-5xl text-center Poppitandfinchsans text-blau">BERK</h3>
                     <a href="https://twitter.com/berkozdemir" className="text-center text-4xl text-center Poppitandfinchsans underline text-white"> (@berkozdemir)</a>                </div>
-                
                 </div>
-       
-
-
               </div>   
               <div id="contact" className="flex flex-wrap justify-around items-center  mx-6 py-6">
                   <div className=" p-4"><img src="images/letstalk.png" alt="" width="400px" className="feature-image"/></div>
@@ -159,9 +141,6 @@ export default function Home() {
                     Or alternatively, join our <a className="text-black underline font-bold" target="_blank" href="https://discord.gg/8Wk9Hp6UyV">Discord Server</a>.
                     </p>
                   </div>
-       
-
-
               </div>   
           </div>  
     </div>  
